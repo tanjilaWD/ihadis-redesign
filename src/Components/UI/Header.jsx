bfaf3f63e842cf3bed73c311713bbd282fe1c2e4
Tanjila Khatun <tanjilabithi12@gmail.com>
2025-10-03 15:26:58 +0600

    icon added

--- src/Components/UI/Header.jsx
+++ src/Components/UI/Header.jsx
@@ -4,14 +4,23 @@ const Header = () => {
         <div>
             <section className="header">
                 <div className="header_Container">
-                    <div className="logo_container">
 
+                    <div className="logo_container">
+                        <img src="../../public/image/header/Headerlogo.png" alt="Logo" />
+                        <h2>আল হাদিস</h2>
                     </div>
+
                     <div className="menu_container">
+                        <ul>
+                            <li><a href="#"><img src="../../public/image/header/home.png" alt="image"/>হোম </a></li>
+                            <li><a href="#">হাদিসে যান </a></li>
+                            <li><a href="#">হাদিস গ্রন্থসমূহ </a></li>
+                            <li><a href="#">আমাদের প্রজেক্ট</a></li>
+                        </ul>
 
                     </div>
                     <div className="btn_container">
-                        
+
                     </div>
                 </div>
 
