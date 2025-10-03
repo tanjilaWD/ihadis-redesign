0e96cda2f93f4056d9da8f9b502723669736de2b
Tanjila Khatun <tanjilabithi12@gmail.com>
2025-10-04 00:47:03 +0600

    new file added

--- src/Components/UI/Search_ber.jsx
+++ src/Components/UI/Search_ber.jsx
@@ -2,7 +2,11 @@ const Search_ber = () => {
     return (
        <section className="search_ber">
             <div className="search_box">
+
+                <div className="search">
+                    <img src="../../public/image/search_ber/search.png" alt="Image" />
                 <input type="text" placeholder="হাদিস খুঁজুন..." />
+                 </div>
                 <span className="arabic">بِسْمِ ٱللَّٰهِ ٱلرَّحْمَٰنِ ٱلرَّحِيمِ</span>
                 <button className="filter_btn">☰</button>
                 <button className="search_btn"> Search</button>
