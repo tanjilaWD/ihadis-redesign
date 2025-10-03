585629428d6e211e3275ad489d5ecb36aea0a012
Tanjila Khatun <tanjilabithi12@gmail.com>
2025-10-03 23:23:42 +0600

    new

--- src/Components/UI/Search_ber.jsx
+++ src/Components/UI/Search_ber.jsx
@@ -1,7 +1,19 @@
 const Search_ber = () => {
     return (
        <section className="search_ber">
-        
+            <div className="search_box">
+                <input type="text" placeholder="হাদিস খুঁজুন..." />
+                <span className="arabic">بِسْمِ ٱللَّٰهِ ٱلرَّحْمَٰنِ ٱلرَّحِيمِ</span>
+                <button className="filter_btn">☰</button>
+                <button className="search_btn"> Search</button>
+            </div>
+
+            <div className="background-decorations">
+                <div className="mosque"></div>
+                <div className="desert_ground"></div>
+
+            </div>
+       
 
        </section>
     );
