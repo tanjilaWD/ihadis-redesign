ec5f72218b5d2c213ac97051bdc1e8319230a19b
Tanjila Khatun <tanjilabithi12@gmail.com>
2025-10-04 10:56:25 +0600

    image added

--- src/Components/UI/App_Store.jsx
+++ src/Components/UI/App_Store.jsx
@@ -4,11 +4,39 @@ const App_Store = () => {
         <section className="app_store">
             <div className="store">
                 <div className="store_left">
-                    <div className="image_side">
 
+                    <div className="image_side">
+                           <img className="screen_img" src="../../public/image/app_store/Screen.png" alt="Image" />
                     </div>
+
                     <div className="content_side">
-                        
+                        <div className="cont">
+                            <h2>আল হাদিস সাইটে আপনাকে স্বাগতম</h2>
+                            <p>আল হাদিস এপের মাধ্যমে হাদিস পড়া, বুকমার্ক, অনলাইন সিংকিং সহ আর ও অনেক কিছু সহজেই পারবেন।</p>
+                        </div>
+
+                        <div className="img">
+                            <h2>ডাউনলোড করুন <br />আল হাদিস মোবাইল আপ </h2>
+                            <div className="play">
+                                <div className="apple">
+                                    <img src="../../public/image/app_store/WhiteBG.png" alt="Image" />
+                                </div>
+
+                                <div className="google">
+                                    <div className="icon">
+                                        <img src="../../public/image/app_store/icon.png" alt="Image" />
+                                    </div>
+                                    <div className="text">
+                                        <img src="../../public/image/app_store/get.png" alt="Image" />
+                                        <img src="../../public/image/app_store/GooglePlay.png" alt="Image" />
+
+                                    </div>
+
+                                </div>
+                            </div>
+
+                        </div>
+
                     </div>
 
 
