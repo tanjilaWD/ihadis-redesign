26fcce656659f4e97091151c3425c1b16005bc5a
Tanjila Khatun <tanjilabithi12@gmail.com>
2025-10-05 14:43:32 +0600

    Subject wise css code added

--- src/Components/UI/Subjectwise.jsx
+++ src/Components/UI/Subjectwise.jsx
@@ -6,13 +6,72 @@ const Subjectwise = () => {
         <div className="wise_container">
             <div className="wiseContent">
                 <h2>বিষয়ভিত্তিক হাদিস</h2>
-                <p>বিষয়ভিত্তিক হাদীস হচ্ছে হাদীসের এমন সংকলন যা বর্ণনাকারী বা সনদ দ্বারা নয় বরং বিষয় অনুসারে সজ্জিত। এর মাধ্যমে ইসলামের নির্দিষ্ট বিষয়গুলোর উপর সহজে জ্ঞানার্জন সম্ভব হয়। এই সঙ্কলন আলেমগণ ও শিক্ষকদের জন্যও সহায়ক হতে পারে যারা নির্দিষ্ট বিষয়ে শিক্ষা প্রদান করতে চাইছেন।</p>
+                <p>বিষয়ভিত্তিক হাদীস হচ্ছে হাদীসের এমন সংকলন যা বর্ণনাকারী বা সনদ দ্বারা নয় বরং বিষয় অনুসারে সজ্জিত। এর <br /> মাধ্যমে ইসলামের নির্দিষ্ট বিষয়গুলোর উপর সহজে জ্ঞানার্জন সম্ভব হয়। এই সঙ্কলন আলেমগণ ও শিক্ষকদের  <br /> সহায়ক হতে পারে যারা নির্দিষ্ট বিষয়ে শিক্ষা প্রদান করতে চাইছেন।</p>
 
             </div>
             <div className="wiseImage">
-                
+                <div className="wiseBox">
+                <div className="wiseText">
+                    <h4>আকিদা</h4>
+                    <p>হাদিসের রেঞ্জ ৭</p>
+                </div>
+                <div className="wiseImg">
+                    <img src="../../public/image/wise/img.png" alt="Image" />
+                </div>
+                </div>
+                <div className="wiseBox">
+                <div className="wiseText">
+                    <h4>ঈমান</h4>
+                    <p>হাদিসের রেঞ্জ ৫</p>
+                </div>
+                <div className="wiseImg">
+                    <img src="../../public/image/wise/img1.png" alt="Image" />
+                </div>
+                </div>
+                <div className="wiseBox">
+                <div className="wiseText">
+                    <h4>পবিত্রতা</h4>
+                    <p>হাদিসের রেঞ্জ ১২</p>
+                </div>
+                <div className="wiseImg">
+                    <img src="../../public/image/wise/img2.png" alt="Image" />
+                </div>
+                </div>
+                <div className="wiseBox">
+                <div className="wiseText">
+                    <h4>হাদিসের গল্প</h4>
+                    <p>হাদিসের রেঞ্জ ৩১</p>
+                </div>
+                <div className="wiseImg">
+                    <img src="../../public/image/wise/img3.png" alt="Image" />
+                </div>
+                </div>
+                <div className="wiseBox">
+                <div className="wiseText">
+                    <h4>সালাত</h4>
+                    <p>হাদিসের রেঞ্জ ৩৮</p>
+                </div>
+                <div className="wiseImg">
+                    <img src="../../public/image/wise/img4.png" alt="Image" />
+                </div>
+                </div>
+                <div className="wiseBox">
+                <div className="wiseText">
+                    <h4>দান সদকাহ</h4>
+                    <p>হাদিসের রেঞ্জ ১</p>
+                </div>
+                <div className="wiseImg">
+                    <img src="../../public/image/wise/img5.png" alt="Image" />
+                </div>
+                </div>
+                </div>
 
-            </div>
+                <button className="wise_btn">
+                <img src="../../public/image/wise/Vector.png" alt="Book Image" />
+                <h4>সকল ক্যাটাগরি দেখুন</h4>
+                <img src="../../public/image/wise/Frame.png" alt="Book Image" />
+
+            </button>
         </div>
 
        </section>
